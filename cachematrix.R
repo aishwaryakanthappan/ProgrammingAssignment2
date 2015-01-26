744f78a7dec604372f686c7a70d2284e93b58c6a
aishwaryakanthappan <aishy.kandy@gmail.com>
2015-01-25 16:27:08 -0800

    changes for hw

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  if("MASS" %in% rownames(installed.packages()) == FALSE){
+    message("Install MASS....")
+    install.packages("MASS")
+    library("MASS")
+  }
+  
+  inverseMatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    inverseMatrix <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) inverseMatrix <<- inv
+  getInverse <- function() inverseMatrix
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
@@ -12,4 +29,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+ 
+  if("MASS" %in% rownames(installed.packages()) == FALSE){
+    message("Install MASS.....")
+    install.packages("MASS")
+    library("MASS")
+  }  
+  
+  invMx <- x$getInverse()
+  
+  if(!is.null(inverseMatrix)) {
+    message("getting cached data")
+    return(inverseMatrix)
+  }
+  
+  data <- x$get()
+  invMx <- ginv(data)
+  x$setInverse(inverseMatrix)
+  
+  inverseMatrix
 }
